c333668a43186aa41f1ef120522206eeaa99258a
Soe Thura <Soe Thura>
2016-03-11 16:40:38 +0630

    add text2image converter

--- background.js
+++ background.js
@@ -4,8 +4,12 @@ chrome.storage.sync.get("data", function(items) {
       var enableMUA = items.data;
       if(enableMUA == "disable") {
         chrome.browserAction.setIcon({path:"icon_disabled.png"});
+        chrome.browserAction.setBadgeText({text: "Off"});
+        chrome.browserAction.setBadgeBackgroundColor({color: "#34A7C1"});
       } else {
-        chrome.browserAction.setIcon({path:"icon.png"});
+        chrome.browserAction.setIcon({path:"icon19.png"});
+        chrome.browserAction.setBadgeText({text: "On"});
+        chrome.browserAction.setBadgeBackgroundColor({color: "#ea2839"});
       }
     } 
 });
