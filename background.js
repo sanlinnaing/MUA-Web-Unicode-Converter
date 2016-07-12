cacaf17b8e8741de13b1534ac05dad5d47a056ae
San Lin Naing <sanlinnaing2008@gmail.com>
2016-07-12 10:31:38 +0630

    fixed color code incorrect

--- background.js
+++ background.js
@@ -5,11 +5,11 @@ chrome.storage.sync.get("data", function(items) {
       if(enableMUA == "disable") {
         chrome.browserAction.setIcon({path:"icon_disabled.png"});
         chrome.browserAction.setBadgeText({text: "Off"});
-        chrome.browserAction.setBadgeBackgroundColor({color: "#34A7C1"});
+        chrome.browserAction.setBadgeBackgroundColor({color: "#ea2839"});
       } else {
         chrome.browserAction.setIcon({path:"icon19.png"});
         chrome.browserAction.setBadgeText({text: "On"});
-        chrome.browserAction.setBadgeBackgroundColor({color: "#ea2839"});
+        chrome.browserAction.setBadgeBackgroundColor({color: "#34A7C1"});
       }
     } 
 });
